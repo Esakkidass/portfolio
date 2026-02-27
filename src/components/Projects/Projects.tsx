f8a81eb737732af2fae0ec2003adb656b8848bd6
Esakki Dass <esakkidass979@gmail.com>
2026-02-27 17:29:13 +0530

    projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -56,7 +56,7 @@ const Projects = () => {
 
   return (
     <section id="projects" className="projects">
-      <h2 className="section-title">My Projects</h2>
+      <h2 className="section-title">Projects</h2>
 
       <div className="project-grid">
         {projectList.map((project, index) => (
